72d2f2b962b896c83f8d5faa546062eb4fbf8e44
erdem koral <koral.erdem@gmail.com>
2017-02-21 13:00:25 -0800

    final changes

--- game.js
+++ game.js
@@ -22,137 +22,21 @@ while (userName === ""){
 }
 
 
-
-console.log("Asking user Question1");
-function questionOne() {
-  var question1 = prompt("True or False? Coffee in the United States is only grown in Hawaii and Puerto Rico.");
-  var result1;
-
-  if ((question1.toLowerCase() === "true")||(question1.toLowerCase() === "t")){
-    //alert("Great!! " + userName + " ,you got it.");
-    result1= ("Correct!!");
+//questions 1 to 4
+function runQuestions(){
+var count =0;
+var key = ["false","true","false","true"];
+for (i=0; i<key.length; i++){
+  var radioButtonName = "answer" +(i+1);
+  var answer = document.forms[0].elements[radioButtonName].value;
+  var finalScoreBoard = document.getElementById("scoreboard" +(i+1));
+  console.log(finalScoreBoard);
+  if(answer === key[i]){
     count++;
-  } else{
-    //alert("Sorry:( " + userName +" ,you missed this one.");
-    //result1 = ("Incorrect");
-    result1= ("Incorrect!!");
-  }
+    finalScoreBoard.innerHTML += "<div class= 'crt'>Correct!!</div>" +            "<img class= 'icon' src='plus.png'>";
 
-  var el = document.getElementById("answer1");
-  el.textContent = result1;
-};
-
-questionOne();
-
-
-
-console.log("Asking user Question2");
-function questionTwo() {
-  var question2 = prompt("True or False? Robusta coffee beans have three times as much caffeine than Arabica beans, but our of less quality.");
-  var result2;
-
-  if ((question2.toLowerCase() === "false")||(question2.toLowerCase() === "f")){
-    //alert("Great!! " + userName + ". Your answer is correct.");
-    result2 = ("Correct!!");
-    count++;
-  } else{
-    //alert("Sorry:( " + userName +". It is only twice as much.");
-    result2 = ("Incorrect!!");
-  }
-
-  var el = document.getElementById('answer2');
-  el.textContent = result2;
-};
-
-questionTwo();
-
-
-console.log("Asking user Question3");
-function questionThree() {
-  var question3 = prompt("True or False? Black coffee with no additives contains no calories.");
-  var result3;
-
-  if ((question3.toLowerCase() === "true")||(question3.toLowerCase() === "t")){
-    count++;
-    result3 = ("Correct!!");
-  } else{
-    result3 = ("Incorrect!!");
-  }
-
-  var el = document.getElementById('answer3');
-  el.textContent = result3;
-};
-questionThree();
-
-
-console.log("Asking user Question4.");
-function questionFour() {
-  var question4 = prompt("How many cups of coffee does an avereaga person drink a day?");
-  var result4 = question4
-
-  if (question4 ==2){
-    count++;
-    result4 = ("Correct!!");
-  } else if (question4 > 2) {
-    result4 = ("Incorrect.Your answer is too high!!");
-  } else {
-    result4 = ("Incorrect.Your answer is too low!!");
-  }
-
-  var el = document.getElementById('answer4');
-  el.textContent = result4;
-};
-questionFour();
-
-
-console.log("Asking user Question5.");
-function questionFive() {
-  while(question5 !== 33 ){
-    var question5 = prompt("How old am I?");
-    question5 = parseInt(question5);
-    if (question5 > 33){
-      alert("Your guess is too high. Try again.");
-    } else if (question5 < 33){
-      alert("Your guess is too low. Try again.");
-    }
-
-    var msg;
-    if(question5 === 33){
-      msg = ("Correct!!");
-      count++;
-    }
-    var el = document.getElementById('answer5');
-    el.textContent =question5 + " is " +msg;
-  }
-};
-questionFive();
-
-console.log(question6);
-var question6 = prompt("What is my favorite coffe drink?");
-var result6 = question6
-var coffee = ["espresso","cappucino","mocha"];
-for(i=0; i<3; i++){
-  if (coffee[i]===question6.toLowerCase()){
-  result6=("Correct!!");
-  count++;
-}else{
-  result6=("Incorrect!!");
+  }else{
+    finalScoreBoard.innerHTML += "<div class= 'wrg'>Incorrect!!</div>" +          "<img class= 'icon' src='delete.png'>" ;
 }
-var el = document.getElementById('answer6');
-el.textContent = result6;
-};
-
-
-console.log("Showing the total score.")
-var score;
-if (count === 6) {
-  score = (" Perfect Score!!! Your total score is " + count + "!!");
-} else if (3< count <6) {
-  score = ("Not Bad!! Your total score is " + count + "!!");
-} else {
-  score = ("You know nothing Jon Snow!! Your total score is " + count + "!!");
 }
-
-
-var doc = document.getElementById('scoreBoard');
-doc.textContent = score;
+}
